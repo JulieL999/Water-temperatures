275889cbad63b5c9fe775dd6e3cc0a90c1c0cd02
filipped <82341252+FilippEder@users.noreply.github.com>
2023-09-28 20:28:30 +0200

    merging

--- app/src/main/java/com/example/watertemperatures/MainActivity.kt
+++ app/src/main/java/com/example/watertemperatures/MainActivity.kt
@@ -55,7 +55,6 @@ class MainActivity : AppCompatActivity(){
             intent = Intent(applicationContext, ClosestPlaces::class.java)
             startActivity(intent)
         }
-    }
 
         val bubble1 = findViewById<ImageView>(R.id.bubble1)
         val bubble2 = findViewById<ImageView>(R.id.bubble2)
@@ -83,24 +82,7 @@ class MainActivity : AppCompatActivity(){
     }
 
     // used for setting entries into database
-    suspend fun databaseAcess(){
-        db = Room.databaseBuilder(applicationContext, CoordinateDatabase::class.java,"Coordinates")
-            .build()
-        val coordinateDao = db.coordinateDAO()
 
-//        coordinateDao.insertAll(
-//            Coordinate(1,"Worthersee","46.62727831226116","14.110936543942412"),
-//            Coordinate(2,"Keutschachersee","46.58534725975028","14.159639373326728"),
-//            Coordinate(3, "Maltschacersee","46.703241956065085","14.142326232846942"),
-//            Coordinate(4,"Baßgeigensee","46.587253915810955","14.202405700047533"),
-//            Coordinate(5,"Rauschelsee","46.58469136779188","14.220967113932259")
-//        )
-
-        val crd: List<Coordinate> = coordinateDao.getByIds(intArrayOf(1))
-        for(c in crd){
-            Log.d("Room","${c.cid} ${c.name} ${c.latitude} ${c.longitude}")
-        }
-    }
 
 
 
@@ -144,5 +126,23 @@ class MainActivity : AppCompatActivity(){
 
         animatorSet.start()
 
+    }
+    suspend fun databaseAcess(){
+        db = Room.databaseBuilder(applicationContext, CoordinateDatabase::class.java,"Coordinates")
+            .build()
+        val coordinateDao = db.coordinateDAO()
+
+//        coordinateDao.insertAll(
+//            Coordinate(1,"Worthersee","46.62727831226116","14.110936543942412"),
+//            Coordinate(2,"Keutschachersee","46.58534725975028","14.159639373326728"),
+//            Coordinate(3, "Maltschacersee","46.703241956065085","14.142326232846942"),
+//            Coordinate(4,"Baßgeigensee","46.587253915810955","14.202405700047533"),
+//            Coordinate(5,"Rauschelsee","46.58469136779188","14.220967113932259")
+//        )
+
+        val crd: List<Coordinate> = coordinateDao.getByIds(intArrayOf(1))
+        for(c in crd){
+            Log.d("Room","${c.cid} ${c.name} ${c.latitude} ${c.longitude}")
+        }
     }
 }
